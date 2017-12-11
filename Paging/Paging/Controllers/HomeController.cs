daa69d512e3c3cb3673bbe14245dc6bbdfd5a34e
Shruti Singh <Shruti@promact.com>
2017-12-11 17:57:52 +0530

    AssignmentModified

---
+++ Paging/Paging/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using Paging.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Paging.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index(int? page)
+        {
+            var dummyItems = Enumerable.Range(1, 150).Select(x => "Item " + x);
+            var pager = new Pager(dummyItems.Count(), page);
+
+            var viewModel = new Customer
+            {
+                Items = dummyItems.Skip((pager.CurrentPage - 1) * pager.PageSize).Take(pager.PageSize),
+                Pager = pager
+            };
+
+            return View(viewModel);
+        }
+
+
+
+
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        //public ActionResult About()
+        //{
+        //    ViewBag.Message = "Your application description page.";
+
+        //    return View();
+        //}
+
+        //public ActionResult Contact()
+        //{
+        //    ViewBag.Message = "Your contact page.";
+
+        //    return View();
+        //}
+    }
+}
